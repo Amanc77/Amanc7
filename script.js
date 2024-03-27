bbdfed6b9f02e560c34db9481a88ccbe1847ddbb
Aman Kumar Chaurasiya <amanchaurasiya255@gmail.com>
2024-03-27 06:47:38 +0530

    added link of all project

--- script.js
+++ script.js
@@ -48,12 +48,12 @@ window.addEventListener('scroll', scrollActive)
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2000,
-    delay: 200,
+    duration: 1500,
+    delay: 100,
 //     reset: true
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 200}); 
+sr.reveal('.home__social-icon',{ interval: 100}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 50}); 
