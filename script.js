7d85d3bc5e27ee64cc3eeabee9f5577357d0dd58
Aman Kumar Chaurasiya <amanchaurasiya255@gmail.com>
2024-05-23 19:36:09 +0530

    .

--- script.js
+++ script.js
@@ -10,12 +10,11 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/* REMOVE MENU MOBILE */
+
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
@@ -41,7 +40,6 @@ const scrollActive = () =>{
 }
 window.addEventListener('scroll', scrollActive)
 
-// for email
 
 
 /*===== SCROLL REVEAL ANIMATION =====*/
